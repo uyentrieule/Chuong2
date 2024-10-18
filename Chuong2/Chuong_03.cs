69d61943a1e9ee4e432f09bc4037c1908c7c5446
uyentrieule <uyentrieu.31231020264@st.ueh.edu.vn>
2024-10-18 23:15:58 +0700

    Chuong_03

--- Chuong2/Chuong_03.cs
+++ Chuong2/Chuong_03.cs
@@ -6,10 +6,14 @@ namespace TrieuLeUyen_31231020264
         private static void Main(string[] args)
         {
             //Question_01();
-            Question_02();
+            //Question_02();
+            Question_03();
             Console.WriteLine("Press any key to stop");
             Console.ReadKey();
         }
+        /// <summary>
+        /// Convert from degrees Celsius to Kelvin and Fahrenheit
+        /// <summary>
         public static void Question_01()
         {
             Console.WriteLine("Enter Celsius degree: ");
@@ -20,7 +24,28 @@ namespace TrieuLeUyen_31231020264
         }
         public static void Question_02()
         {
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double Surface = 4 * Math.PI * Math.Pow(radius,2);
+            double Volume = 4 / 3 * Math.PI * Math.Pow(radius,3);
+            Console.WriteLine($"The sphere's surface is {Surface}. The sphere's volume is {Volume}");
 
+        }
+        /// <summary>
+        /// Calculates the result of adding, subtracting, multiplying and dividing two numbers entered by the user
+        /// </summary>
+        public static void Question_03()
+        {
+            Console.WriteLine("Enter number a: ");
+             int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number b: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            int f = a / b;
+            int g = a % b;
+            Console.WriteLine($"a + b = {c}. a * b = {d}. a * b = {e}. a / b = {f}. The rest of the division is {g}");
         }
     }
         
